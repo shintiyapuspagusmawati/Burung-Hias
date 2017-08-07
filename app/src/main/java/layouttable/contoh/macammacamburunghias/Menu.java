cacd0460555da901d01e41f1bbc6969cfbb4d184
shintiyapuspagusmawati <151610260@smkassalaambandung.sch.id>
2017-08-07 16:47:31 +0400

    baru 7

--- app/src/main/java/layouttable/contoh/macammacamburunghias/Menu.java
+++ app/src/main/java/layouttable/contoh/macammacamburunghias/Menu.java
@@ -23,6 +23,61 @@ public class Menu extends Activity {
 
             }
         });
+
+        Button knr = (Button) findViewById(R.id.kenari);
+        knr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View pilih) {
+                Intent myIntent = new
+                        Intent(pilih.getContext(), Kenari.class);
+                startActivityForResult(myIntent, 0);
+
+            }
+        });
+
+        Button kc = (Button) findViewById(R.id.kacamata);
+        kc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View pilih) {
+                Intent myIntent = new
+                        Intent(pilih.getContext(), Kacamata.class);
+                startActivityForResult(myIntent, 0);
+
+            }
+        });
+
+        Button br = (Button) findViewById(R.id.branjangan);
+        br.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View pilih) {
+                Intent myIntent = new
+                        Intent(pilih.getContext(), Branjangan.class);
+                startActivityForResult(myIntent, 0);
+
+            }
+        });
+
+        Button cr = (Button) findViewById(R.id.cucakrowo);
+        cr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View pilih) {
+                Intent myIntent = new
+                        Intent(pilih.getContext(), Cucakrowo.class);
+                startActivityForResult(myIntent, 0);
+
+            }
+        });
+
+        Button cb = (Button) findViewById(R.id.ciblek);
+        cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View pilih) {
+                Intent myIntent = new
+                        Intent(pilih.getContext(), Ciblek.class);
+                startActivityForResult(myIntent, 0);
+
+            }
+        });
     }
 
 }
