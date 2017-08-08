bff8dd30e56c0d83e81fb0122a4163e58a97a988
shintiyapuspagusmawati <151610260@smkassalaambandung.sch.id>
2017-08-08 12:24:13 +0400

    alhamdulillah beress gusti

--- app/src/main/java/layouttable/contoh/macammacamburunghias/Kenari.java
+++ app/src/main/java/layouttable/contoh/macammacamburunghias/Kenari.java
@@ -1,14 +1,111 @@
 package layouttable.contoh.macammacamburunghias;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class Kenari extends Activity {
+
+    private Button btnPlay;
+    private Button btnPause;
+    private Button btnStop;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kenari);
+
+        btnPlay = (Button) findViewById(R.id.btnPLAY);
+        btnPause = (Button) findViewById(R.id.btnPAUSE);
+        btnStop = (Button) findViewById(R.id.btnSTOP);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play();
+                btnPlay.setEnabled(false);
+                btnPause.setEnabled(true);
+                btnStop.setEnabled(true);
+            }
+        });
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pause();
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stop();
+            }
+        });
+    }
+    /** State Awal / Pertama Dijalankan */
+    public void stateAwal(){
+        btnPlay.setEnabled(true);
+        btnPause.setEnabled(false);
+        btnStop.setEnabled(false);
+    }
+
+    /** Dijalankan Oleh Tombol Play */
+    private void play() {
+        /** Memanggil File MP3 "indonesiaraya.mp3" */
+        mp = MediaPlayer.create(this, R.raw.kenari);
+
+        try {
+            mp.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /** Menjalankan Audio */
+        mp.start();
+
+        /** Penanganan Ketika Suara Berakhir */
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stateAwal();
+            }
+        });
+    }
+
+    /** Dijalankan Oleh Tombol Pause */
+    public void pause(){
+        if(mp.isPlaying()){
+            if(mp!=null){
+                mp.pause();
+
+            }
+        } else {
+            if(mp!=null){
+                mp.start();
+
+            }
+        }
+    }
+
+    /** Dijalankan Oleh Tombol Stop */
+    public void stop(){
+        mp.stop();
+
+        try{
+            mp.prepare();
+            mp.seekTo(0);
+        }catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+        stateAwal();
     }
 
 }
